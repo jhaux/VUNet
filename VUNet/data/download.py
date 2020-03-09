e21ff9178f0faac4ceec0372097af4764b70b77a
Johannes Haux <jo.mobile.2011@gmail.com>
2020-03-09 17:57:35 +0100

    :tada: Adds super tiny debug dataset

--- VUNet/data/download.py
+++ VUNet/data/download.py
@@ -48,7 +48,7 @@ def _save_response_content(response, destination, size=None):
                 f.write(chunk)
 
 
-def download_prjoti(zip_path):
+def download_prjoti(zip_path, split):
     '''Downloads the tiny Prjoti_J split of the Prjoti Dataset to
     :attr:`store_root`. Also sets up the correct config for this dataset.
     
@@ -56,11 +56,20 @@ def download_prjoti(zip_path):
     ---------
     store_root : str
         Path to the folder, where the dataset is supposed to be stored.
-    
+    split : str
+        Which split of the Prjoti_J Dataset to install: Can be either
     '''
 
-    prjoti_id = '186CE_r0gfgaF6CznBg3zu9_HxioeZvM-'
-    _download_file_from_google_drive(prjoti_id, zip_path, size=1589507114)
+    assert split in ['default', 'tiny']
+
+    if split == 'default':
+        prjoti_id = '186CE_r0gfgaF6CznBg3zu9_HxioeZvM-'
+        size = 1589507114
+    else:
+        prjoti_id = '1QfaY_keSKeU4_p0GCjKPFtPO8eosnoFE'
+        size = 3254064 
+
+    _download_file_from_google_drive(prjoti_id, zip_path, size=size)
 
 
 def extract_prjoti(zip_path):
@@ -104,7 +113,8 @@ def prep_config(store_root):
     with open(meta_path, 'r') as mf:
         content = yaml.safe_load(mf.read())
 
-    content['loader_kwargs']['frame']['root'] = store_root
+    if 'frame' in content['loader_kwargs']:
+        content['loader_kwargs']['frame']['root'] = store_root
     content['loader_kwargs']['crop']['root'] = store_root
 
     with open(meta_path, 'w+') as mf:
@@ -118,16 +128,30 @@ def ask_store_path(default_root=None):
     print('====== Prjoti_J Dataset Download =====')
     print('======================================')
     print()
+    print( '>> ctrl+c to abort')
+    print()
     print('It seems you do not have the Prjoti_J dataset on you system. This '
           'dialogue will guide you through the steps necessary to download '
-          'it. The dataset needs 1.6GB when extracted and 3.2GB temporarily '
-          'during installation.')
+          'it.')
+    print()
+    print('You can choose between two version of the dataset:')
+    print('  (1) The default version, which needs 1.6GB when extracted and '
+          '3.2GB temporarily ' 'during installation.')
+    print('  (2) The super tiny version, which needs 3.3MB when extracted and '
+          '6.6MB temporarily ' 'during installation.')
+    print()
+    print('Please enter your choice by typing 1 or 2:')
+
+    split = input()
+    while split not in ['1', '2']:
+        print('Please enter either 1 or 2')
+        split = input()
+    split = 'default' if split == '1' else 'tiny'
+
     print()
     print('Please enter the absolute path, where you want Prjoti_J to be '
           'stored. Make sure it is a path to a possibly empty folder, which '
           'already exists.')
-    print()
-    print( '>> ctrl+c to abort')
     print()
 
     if default_root is not None:
@@ -138,20 +162,21 @@ def ask_store_path(default_root=None):
     if store_url == '':
         store_url = default_root
 
-    store_url = os.path.join(store_url, 'Prjoti_J.zip')
+    suffix = '' if split == 'default' else '_tiny'
+    store_url = os.path.join(store_url, f'Prjoti_J{suffix}.zip')
 
     print()
     print(f'File will be saved to `{store_url}`.')
 
-    return store_url
+    return store_url, split
 
 
 def prjoti_installer(default_root=None):
     '''Donwloads and installs the Prjoti_J dataset.'''
 
-    zip_path = ask_store_path(default_root)
+    zip_path, split = ask_store_path(default_root)
 
-    download_prjoti(zip_path)
+    download_prjoti(zip_path, split)
     extract_prjoti(zip_path)
 
     root_path = os.path.dirname(zip_path)
